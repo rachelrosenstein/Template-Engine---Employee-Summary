10341290a677a335a26675861eb1846812a7dde9
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2020-11-01 13:53:03 -0500

    trying to get tests to work

--- Develop/app.js
+++ Develop/app.js
@@ -10,80 +10,101 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const teamMembers = [];
 
-createTeam();
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+// var Employee = require("./lib/Employee");
+// var Engineer = require("./lib/Engineer");
+// var Intern = require("./lib/Intern");
+// var Manager = require("./lib/Manager");
 
-function createTeam() {
-    inquirer.prompt([{
-        message: "Enter team member's name",
-        name: "name"
-    },
-    {
-        type: "list",
-        message: "Select team member's role",
-        choices: [
-            "Engineer",
-            "Intern",
-            "Manager",
-        ],
-        name: "role"
-    },
-    {
-        message: "Enter team member's id",
-        name: "id"
-    },
-    {
-        message: "Enter team member's email address",
-        name: "email"
-    }])
+const collectInputs = async (inputs = []) => {
+    const prompts = [
+        {
+            type: "input",
+            message: "What is the employee's name?",
+            name: "employeeName"
+        },
+        {
+            type: "input",
+            message: "What is the employee's id number?",
+            name: "idNumber"
+        },
+        {
+            type: "list",
+            message: "What is the employee's job title?",
+            name: "jobTitle",
+            choices: [
+                "Employee",
+                "Manager",
+                "Engineer",
+                "Intern"
+            ]
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Enter the manager's office number:",
+            when: (answers) => answers.jobTitle === 'Manager'
+        },
+        {
+            type: "input",
+            message: "What school does/did the intern attend?",
+            name: "school",
+            when: (answers) => answers.jobTitle === 'Intern'
+        },
+        {
+            type: "input",
+            message: "What is the engineer's GitHub username?",
+            name: "githubUsername",
+            when: (answers) => answers.jobTitle === 'Engineer'
+        },
+        {
+            type: 'confirm',
+            name: 'again',
+            message: 'Enter another employee?',
+            default: true
+        }
+    ];
 
-        .then(function ({ name, role, id, email }) {
-            let roleName = "";
-            if (role === "Engineer") {
-                roleName = "Github username";
-            }
-            else if (role === "Intern") {
-                roleName = "School Name";
-            }
-            else {
-                roleName = "Office Phone Number";
-            }
-
-            inquirer.prompt({
-                message: `Enter Team Member's ${roleName}`,
-                name: "roleName"
-            },
-                {
-                    Type: "list",
-                    name: "addMembers",
-                    messge: "Would you like to add a New Team Member?",
-                    choices: [
-                        "yes",
-                        "no"
-                    ],
-                })
-                .then(function ({ roleName, AddMember }) {
-                    let newMember = "";
-
-                    if (role === "Engineer") {
-                        newMember = new Engineer(name, id, email, roleName);
-                    } else if (role === "Intern") {
-                        newMember = new Intern(name, id, email, roleName);
-                    } else {
-                        newMember = new Manager(name, id, email, roleName);
-                    }
-                    teamMembers.push(newMember);
-                    console.log(teamMembers);
-                    if (AddMember === "yes") {
-                        createTeam();
-                    };
-                }
-                );
-        });
+    const { again, ...answers } = await inquirer.prompt(prompts);
+    const newInputs = [...inputs, answers];
+    return again ? collectInputs(newInputs) : newInputs;
 };
 
+const writeHTML = async () => {
+    const inputs = await collectInputs();
+    for (i = 0; i < inputs.length; i++) {
+        console.log(inputs[i]);
+        const employeeHTML = `<div class="card">
+        <div class="card-body">
+          ${JSON.stringify(inputs[i])}
+        </div>
+        </div>`
+        const html = `<!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <title>Document</title>
+        </head>
+        
+        <body>
+            ${employeeHTML}
+        </body>
+        
+        </html>`
+        writeFileAsync("team.html", html)
+    }
 
+
+};
+
+writeHTML();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
